ba4c106194b78e1eb1cb485514b35a24f8dcd8bb
mulid8273-design <mulid8273@gmail.com>
2025-12-01 07:46:28 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
-const fileInput = document.getElementById("fileInput");
-const fileName = document.getElementById("fileName");
-
-fileInput.addEventListener("change", () => {
-    fileName.innerText = fileInput.files.length
-        ? fileInput.files[0].name
-        : "선택된 파일 없음";
-});
-
-function convertToJPG() {
-    alert("PNG → JPG 변환 기능 준비 중!");
+function convertToJpg() {
+alert("PNG → JPG 변환 기능 준비중입니다.");
 }
 
-function convertToWEBP() {
-    alert("PNG → WEBP 변환 기능 준비 중!");
+
+function convertToWebp() {
+alert("PNG → WEBP 변환 기능 준비중입니다.");
 }
+
 
 function resizeImage() {
-    alert("이미지 리사이즈 기능 준비 중!");
+alert("이미지 리사이즈 기능 준비중입니다.");
 }
 
-function imageToPDF() {
-    alert("이미지 → PDF 기능 준비 중!");
+
+function imageToPdf() {
+alert("이미지 → PDF 기능 준비중입니다.");
 }

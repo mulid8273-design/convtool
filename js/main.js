a3e1a147e78ab7789f2de906f219a1f7aa13030f
mulid8273-design <mulid8273@gmail.com>
2025-12-06 21:25:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-// 공통 유틸 + 다운로드 함수
-
-// 브라우저에서 파일 다운로드
-export function downloadFile(blob, filename) {
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  a.remove();
-  URL.revokeObjectURL(url);
-}
-
-// DOM helper
-export function $(id) {
-  return document.getElementById(id);
-}
+// 공통 기능 (현재는 예비용)
+console.log("MULID main.js 로딩됨");
